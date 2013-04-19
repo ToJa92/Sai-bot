e5a43ecf8c6d1534016f5e180731f2e9a0d02048
Tobias Jansson <ToJa92@gmail.com>
2013-04-19 11:32:36 +0200

    Addad a parameter scope to the eval()-functions, as well as starting to make some changes in PrintNode.

--- class.rb
+++ class.rb
@@ -49,9 +49,9 @@ class ProgramRoot
     @scope = Scope.new
   end
 
-  def eval(stack)
+  def eval
     @stmt_list.each do |stmt|
-      stmt.eval(stack)
+      stmt.eval(@scope)
     end
   end
 end
@@ -64,9 +64,12 @@ class PrintNode
     @input = input
   end
 
-  # TODO: must somehow get variable names etc. Maybe current scope can be passed to eval?
-  def eval
+  def eval(scope)
     @input.each do |stmt|
+      val = nil;
+      if stmt.is_a? IdentifierNode then
+        val = scope.get_var(stmt.name)
+      end
       puts(stmt)
     end
     true
@@ -80,7 +83,7 @@ class InputNode
   end
 
   # TODO: stmt should probably be looked-up in the scope so we assign the correct var
-  def eval
+  def eval(scope)
     @input.each do |stmt|
       stmt = gets
     end
@@ -93,7 +96,7 @@ class InsertNode
     @expr = expr
   end
 
-  def eval
+  def eval(scope)
     # getVarById(@id) << expr.eval
   end
 end
@@ -105,7 +108,7 @@ class RemoveNode
     @id, @index = id, index
   end
 
-  def eval
+  def eval(scope)
     # array indexes can have a -(minus) prepended which will cause Ruby
     # to select the element from the right side
     # raise "index out of bounds" if @index.abs > lst.size
@@ -121,7 +124,7 @@ class InsertNode
     @id, @expr, @index = id, expr, index
   end
 
-  def eval
+  def eval(scope)
     #lst = getVarById(@id, scope)
     #raise "index out of bounds" if @index.abs > lst.size
     #lst.insert(@index, @expr.eval(scope))
@@ -133,7 +136,7 @@ class AtNode
     @id, @index = id, index
   end
 
-  def eval
+  def eval(scope)
     #lst = getVarById(@id, scope)
     #raise "index out of bounds" if @index.abs > lst.size
     #return lst[@index]
@@ -145,7 +148,7 @@ class LengthNode
     @id = id
   end
 
-  def eval
+  def eval(scope)
     #return getVarById(@id, scope).size
   end
 end
@@ -155,7 +158,7 @@ class IfStmtNode
     @cond, @stmts = cond, stmts
   end
 
-  def eval
+  def eval(scope)
     if @cond.eval then
       @stmts.each do |stmt|
         stmt.eval
@@ -169,7 +172,7 @@ class IfElseStmtNode
     @cond, @stmts = cond, stmts
   end
 
-  def eval
+  def eval(scope)
     if @cond.eval then
       @stmts.each do |stmt|
         stmt.eval
@@ -183,7 +186,7 @@ class ElseStmtNode
     @stmts = stmts
   end
 
-  def eval
+  def eval(scope)
     @stmts.each do |stmt|
       stmt.eval
     end
@@ -197,7 +200,7 @@ class ForStmtNode
     @assign_stmt, @test_stmt, @incr_expr, @stmts = assign_stmt, test_stmt, incr_expr, stmts
   end
 
-  def eval
+  def eval(scope)
     @new_scope = createChildScope(scope)
     @assign_stmt.eval(@new_scope) # should somehow be added to the scope in which test_stmt is eval'd
     while @test_stmt.eval(@new_scope) do
@@ -215,7 +218,7 @@ class WhileStmtNode
     @expr, @stmts = expr, stmts
   end
 
-  def eval
+  def eval(scope)
     @new_scope = createChildScope(scope)
     while @expr.eval(@new_scope) do
       @stmts.each do |stmt|
@@ -226,7 +229,7 @@ class WhileStmtNode
 end
 
 class ReturnNode
-  def eval
+  def eval(scope)
     #getCurrScopeFunc.return?
   end
 end
@@ -239,18 +242,20 @@ class IntegerNode
     @num = num
   end
 
-  def eval
+  def eval(scope)
     @num
   end
 end
 
 class IdentifierNode
-  def initialize(id, val = nil)
-     @id, @val = id, val
+  attr_reader :name
+
+  def initialize(name, val = nil)
+     @name, @val = name, val
   end
 
-  def eval
-    scope.addVar(@id, @val)
+  def eval(scope)
+    scope.add_var(@name, @val)
   end
 end
 
@@ -259,7 +264,7 @@ class StringNode
     @val = val
   end
 
-  def eval
+  def eval(scope)
     @val
   end
 end
@@ -269,7 +274,7 @@ class BoolNode
     @val = val
   end
 
-  def eval
+  def eval(scope)
     @val
   end
 end
@@ -279,7 +284,7 @@ class ListNode
     @val = val
   end
 
-  def eval
+  def eval(scope)
     @val
   end
 end
@@ -291,7 +296,7 @@ class AndNode
     @op1, @op2 = op1, op2
   end
 
-  def eval
+  def eval(scope)
     @op1.eval(scope) and @op2.eval(scope)
   end
 end
@@ -301,7 +306,7 @@ class OrNode
     @op1, @op2 = op1, op2
   end
 
-  def eval
+  def eval(scope)
     @op1.eval(scope) or @op2.eval(scope)
   end
 end
@@ -311,7 +316,7 @@ class NotNode
     @op1 = op1
   end
 
-  def eval
+  def eval(scope)
     not @op1.eval(scope)
   end
 end
@@ -321,7 +326,7 @@ class ComparisonNode
     @op1, @oper, @op2 = op1, oper, op2
   end
 
-  def eval
+  def eval(scope)
     @op1.eval @oper @op2.eval
   end
 end
@@ -331,7 +336,7 @@ class AddNode
     @op1, @op2 = op1, op2
   end
 
-  def eval
+  def eval(scope)
     @op1.eval + @op2.eval
   end
 end
@@ -343,7 +348,7 @@ class SubtractNode
     @op1, @op2 = op1, op2
   end
 
-  def eval
+  def eval(scope)
     @op1.eval - @op2.eval
   end
 end
@@ -353,7 +358,7 @@ class MultiplyNode
     @op1, @op2 = op1, op2
   end
 
-  def eval
+  def eval(scope)
     @op1.eval * @op2.eval
   end
 end
@@ -363,7 +368,7 @@ class DivisionNode
     @op1, @op2 = op1, op2
   end
 
-  def eval
+  def eval(scope)
     @op1.eval / @op2.eval
   end
 end
